ca423853f4d355942d74d8f48cce39fbe0717fdc
Krishna Gundavaram <deepak.gundavaram@genworth.com>
2020-03-14 23:12:22 -0400

    Working code with day planner

---
+++ script.js
@@ -0,0 +1,165 @@
+$(document).ready(function() {
+  
+
+  const test = true;
+
+  const now = moment().format('MMMM Do YYYY');
+
+  let nowHour24 = moment().format('H');
+  let nowHour12 = moment().format('h');
+  console.log(nowHour12);
+  console.log(nowHour24);
+  console.log(test)
+
+  
+
+  let $dateHeading = $('#navbar-subtitle');
+  $dateHeading.text(now);
+  
+
+  const saveIcon = "./images/save-regular.png"; 
+
+  
+  let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+
+  if (test) { console.log(storedPlans); }
+
+ 
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    planTextArr = new Array(9);
+    planTextArr[4] = " Group lunch ";
+  }
+
+ 
+
+  let $plannerDiv = $('#plannerContainer');
+  
+  $plannerDiv.empty();
+
+  
+
+  for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
+    
+    let $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index',hour);
+  
+    let $col2TimeDiv = $('<div>');
+    $col2TimeDiv.addClass('col-md-2');
+  
+    const $timeBoxSpn = $('<span>');
+    $timeBoxSpn.attr('class','timeBox');
+    
+    let displayHour = 0;
+    let ampm = "";
+    if (hour > 12) { 
+      displayHour = hour - 12;
+      ampm = "pm";
+    } else {
+      displayHour = hour;
+      ampm = "am";
+    }
+    
+    $timeBoxSpn.text(`${displayHour} ${ampm}`);
+
+    $rowDiv.append($col2TimeDiv);
+    $col2TimeDiv.append($timeBoxSpn);
+
+   
+    let $dailyPlanSpn = $('<input>');
+
+    $dailyPlanSpn.attr('id',`input-${index}`);
+    $dailyPlanSpn.attr('hour-index',index);
+    $dailyPlanSpn.attr('type','text');
+    $dailyPlanSpn.attr('class','dailyPlan');
+
+   
+    $dailyPlanSpn.val( planTextArr[index] );
+    
+    let $col9IptDiv = $('<div>');
+    $col9IptDiv.addClass('col-md-9');
+
+    $rowDiv.append($col9IptDiv);
+    $col9IptDiv.append($dailyPlanSpn);
+    
+    let $col1SaveDiv = $('<div>');
+    $col1SaveDiv.addClass('col-md-1');
+
+    let $saveBtn = $('<i>');
+    $saveBtn.attr('id',`saveid-${index}`);
+    $saveBtn.attr('save-id',index);
+    $saveBtn.attr('class',"far fa-save saveIcon");
+    
+   
+    $rowDiv.append($col1SaveDiv);
+    $col1SaveDiv.append($saveBtn);
+console.log(hour);
+console.log($rowDiv);
+    updateRowColor($rowDiv, hour);
+    
+    $plannerDiv.append($rowDiv);
+  };
+
+  function updateRowColor ($hourRow,hour) { 
+
+    // if (test) { console.log("rowColor ",nowHour24, hour); }
+    // if (test) { console.log( hour); }
+
+
+    if ( hour < nowHour24) {
+      if (test) { console.log("lessThan");
+    console.log($hourRow);
+    }
+      $hourRow.css(newFunction(),"lightgrey")
+      
+
+    } else if ( hour > nowHour24) {
+      if (test) { console.log("greaterthan"); }
+      $hourRow.css("background-color","lightgreen")
+    } else {
+      if (test) { console.log("eqaul"); }
+      $hourRow.css("background-color","tomato")
+      $hourRow.css()
+    }
+
+      function newFunction() {
+          return "background-color";
+      }
+  };
+
+  $(document).on('click','i', function(event) {
+    event.preventDefault();  
+
+    if (test) { console.log('click pta before '+ planTextArr); }
+
+    let $index = $(this).attr('save-id');
+
+    let inputId = '#input-'+$index;
+    let $value = $(inputId).val();
+
+    planTextArr[$index] = $value;
+
+
+    if (test) { console.log('value ', $value); }
+    if (test) { console.log('index ', $index); }
+    if (test) { console.log('click pta after '+ planTextArr); }
+
+    $(`#saveid-${$index}`).removeClass('shadowPulse');
+    localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
+  });  
+  
+  $(document).on('change','input', function(event) {
+    event.preventDefault();  
+    if (test) { console.log('onChange'); }
+    if (test) { console.log('id', $(this).attr('hour-index')); }
+
+
+    let i = $(this).attr('hour-index');
+
+    $(`#saveid-${i}`).addClass('shadowPulse');
+  });
+});
